bc16ef8164fad3689a3a0b6af94ce59cf107716f
Peritia <Peritia@peritia.space>
2026-03-03 17:46:07 +0100

    test: add secondary clipboard option

--- nixosVista/homeManager/cliphist/main.nix
+++ nixosVista/homeManager/cliphist/main.nix
@@ -9,8 +9,20 @@
     home.packages = with pkgs; [
       wl-clipboard
       cliphist
+      fzf
       wofi
     ];
+
+    home.file.".local/bin/TermClipboardManager" = {
+      executable = true;
+      text = ''
+        #!/usr/bin/env bash
+        cliphist list | \
+        fzf --preview 'cliphist decode {1} | chafa -' \
+            --preview-window=right:60%:wrap | \
+        cliphist decode | wl-copy
+      '';
+    };
 
     services.cliphist = {
       enable = true;
@@ -35,6 +47,15 @@
       #exec-once = wl-paste --type text --watch cliphist store
       #exec-once = wl-paste --type image --watch cliphist store
       bind = $mainMod, V, exec, cliphist list | wofi --dmenu --prompt "Clipboard" | cliphist decode | wl-copy
+
+      bind = $mainMod SHIFT, V, exec, alacritty --class TermClipboardManager -e TermClipboardManager
+      windowrule = float, class:^(TermClipboardManager)$
+      windowrule = center, class:^(TermClipboardManager)$
+      windowrule = size 1000 600, class:^(TermClipboardManager)$
+      windowrule = animation popin, class:^(TermClipboardManager)$
+      windowrule = rounding 12, class:^(TermClipboardManager)$
+      windowrule = noborder, class:^(TermClipboardManager)$
+
     '';
   };
 }
